469efc3051b3e5ae9337a417a4559d987319fe63
JaredMorillo <jared.morillo@hotmail.com>
2025-11-08 07:55:48 -0500

    Host Documentation

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'CSE 341 API',
     description: 'API Documentation for CSE 341',
   },
-  host: 'localhost:3000',
+  host: ['localhost:3000', 'cse341-vcsw.onrender.com'],
   schemes: ['http', 'https'],
   securityDefinitions: {
     apiKeyAuth: {
